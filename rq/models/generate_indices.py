f2deb2015c848ebae2074b18d615c3bcb83f0cc5
Qiang Zhang <zhangtemplar@gmail.com>
2025-11-07 23:01:42 +0000

    when code book is [512,512,512], faiss rq actually generates [256,256,8]

--- rq/models/generate_indices.py
+++ rq/models/generate_indices.py
@@ -1,6 +1,8 @@
 import collections
+import argparse
 import json
 import logging
+import random
 
 import numpy as np
 import torch
@@ -12,6 +14,9 @@ from torch.utils.data import DataLoader
 
 from datasets import EmbDataset
 from models.rqvae import RQVAE
+
+
+logger: logging.Logger = logging.getLogger(__name__)
 
 import os
 
@@ -41,105 +46,128 @@ def get_collision_item(all_indices_str):
 
     return collision_item_groups
 
-dataset = "Office_Products"
-ckpt_path = ""
-output_dir = f"../data/{dataset}/"
-output_file = f"{dataset}.index.json"
-output_file = os.path.join(output_dir,output_file)
-device = torch.device("cuda:0")
 
-ckpt = torch.load(ckpt_path, map_location=torch.device('cpu'))
-args = ckpt["args"]
-state_dict = ckpt["state_dict"]
+def main(data_path, ckpt_path, output_file, device):
+    ckpt = torch.load(ckpt_path, map_location=torch.device('cpu'))
+    args = ckpt["args"]
+    state_dict = ckpt["state_dict"]
 
 
-data = EmbDataset(args.data_path)
+    data = EmbDataset(data_path)
 
-model = RQVAE(in_dim=data.dim,
-                  num_emb_list=args.num_emb_list,
-                  e_dim=args.e_dim,
-                  layers=args.layers,
-                  dropout_prob=args.dropout_prob,
-                  bn=args.bn,
-                  loss_type=args.loss_type,
-                  quant_loss_weight=args.quant_loss_weight,
-                  kmeans_init=args.kmeans_init,
-                  kmeans_iters=args.kmeans_iters,
-                  sk_epsilons=args.sk_epsilons,
-                  sk_iters=args.sk_iters,
-                  )
+    model = RQVAE(in_dim=data.dim,
+                    num_emb_list=args.num_emb_list,
+                    e_dim=args.e_dim,
+                    layers=args.layers,
+                    dropout_prob=args.dropout_prob,
+                    bn=args.bn,
+                    loss_type=args.loss_type,
+                    quant_loss_weight=args.quant_loss_weight,
+                    kmeans_init=args.kmeans_init,
+                    kmeans_iters=args.kmeans_iters,
+                    sk_epsilons=args.sk_epsilons,
+                    sk_iters=args.sk_iters,
+                    )
 
-model.load_state_dict(state_dict)
-model = model.to(device)
-model.eval()
-print(model)
+    model.load_state_dict(state_dict)
+    model = model.to(device)
+    model.eval()
+    logger.info(model)
 
-data_loader = DataLoader(data,num_workers=args.num_workers,
-                             batch_size=64, shuffle=False,
-                             pin_memory=True)
+    data_loader = DataLoader(data,num_workers=args.num_workers,
+                                batch_size=64, shuffle=False,
+                                pin_memory=True)
 
-all_indices = []
-all_indices_str = []
-prefix = ["<a_{}>","<b_{}>","<c_{}>","<d_{}>","<e_{}>"]
+    all_indices = []
+    all_indices_str = []
+    prefix = ["<a_{}>","<b_{}>","<c_{}>","<d_{}>","<e_{}>"]
 
-for d in tqdm(data_loader):
-    d = d.to(device)
-    indices = model.get_indices(d,use_sk=False)
-    indices = indices.view(-1, indices.shape[-1]).cpu().numpy()
-    for index in indices:
-        code = []
-        for i, ind in enumerate(index):
-            code.append(prefix[i].format(int(ind)))
-
-        all_indices.append(code)
-        all_indices_str.append(str(code))
-    # break
-
-all_indices = np.array(all_indices)
-all_indices_str = np.array(all_indices_str)
-
-for vq in model.rq.vq_layers[:-1]:
-    vq.sk_epsilon=0.0
-# model.rq.vq_layers[-1].sk_epsilon = 0.005
-if model.rq.vq_layers[-1].sk_epsilon == 0.0:
-    model.rq.vq_layers[-1].sk_epsilon = 0.003
-
-tt = 0
-#There are often duplicate items in the dataset, and we no longer differentiate them
-while True:
-    if tt >= 20 or check_collision(all_indices_str):
-        break
-
-    collision_item_groups = get_collision_item(all_indices_str)
-    print(collision_item_groups)
-    print(len(collision_item_groups))
-    for collision_items in collision_item_groups:
-        d = data[collision_items].to(device)
-
-        indices = model.get_indices(d, use_sk=True)
+    for d in tqdm(data_loader):
+        d = d.to(device)
+        indices = model.get_indices(d,use_sk=False)
         indices = indices.view(-1, indices.shape[-1]).cpu().numpy()
-        for item, index in zip(collision_items, indices):
+        for index in indices:
             code = []
             for i, ind in enumerate(index):
                 code.append(prefix[i].format(int(ind)))
 
-            all_indices[item] = code
-            all_indices_str[item] = str(code)
-    tt += 1
+            all_indices.append(code)
+            all_indices_str.append(str(code))
+        # break
+
+    all_indices = np.array(all_indices)
+    all_indices_str = np.array(all_indices_str)
+
+    for vq in model.rq.vq_layers[:-1]:
+        vq.sk_epsilon=0.0
+    # model.rq.vq_layers[-1].sk_epsilon = 0.005
+    if model.rq.vq_layers[-1].sk_epsilon == 0.0:
+        model.rq.vq_layers[-1].sk_epsilon = 0.003
+
+    tt = 0
+    #There are often duplicate items in the dataset, and we no longer differentiate them
+    while True:
+        if tt >= 20 or check_collision(all_indices_str):
+            break
+
+        collision_item_groups = get_collision_item(all_indices_str)
+        logger.info(collision_item_groups)
+        logger.info(len(collision_item_groups))
+        for collision_items in collision_item_groups:
+            d = data[collision_items].to(device)
+
+            indices = model.get_indices(d, use_sk=True)
+            indices = indices.view(-1, indices.shape[-1]).cpu().numpy()
+            for item, index in zip(collision_items, indices):
+                code = []
+                for i, ind in enumerate(index):
+                    code.append(prefix[i].format(int(ind)))
+
+                all_indices[item] = code
+                all_indices_str[item] = str(code)
+        tt += 1
 
 
-print("All indices number: ",len(all_indices))
-print("Max number of conflicts: ", max(get_indices_count(all_indices_str).values()))
+    logger.info(f"All indices number:  {len(all_indices)}")
+    logger.info(f"Max number of conflicts: {max(get_indices_count(all_indices_str).values())}")
 
-tot_item = len(all_indices_str)
-tot_indice = len(set(all_indices_str.tolist()))
-print("Collision Rate",(tot_item-tot_indice)/tot_item)
+    tot_item = len(all_indices_str)
+    tot_indice = len(set(all_indices_str.tolist()))
+    logger.info(f"Collision Rate {(tot_item-tot_indice)/tot_item}")
 
-all_indices_dict = {}
-for item, indices in enumerate(all_indices.tolist()):
-    all_indices_dict[item] = list(indices)
+    all_indices_dict = {}
+    for item, indices in enumerate(all_indices.tolist()):
+        all_indices_dict[item] = list(indices)
 
 
 
-with open(output_file, 'w') as fp:
-    json.dump(all_indices_dict,fp)
+    with open(output_file, 'w') as fp:
+        json.dump(all_indices_dict,fp)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Index")
+
+    parser.add_argument('--dataset', type=str, help='full path to dataset, which is a npy file')
+    parser.add_argument('--output_file', type=str, help='full path to output file, which is a json file')
+    parser.add_argument('--ckpt_path', type=str, help='full path to checkpoint file, which is a pth file')
+    parser.add_argument('--device', type=str, default="cuda:0")
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    """fix the random seed"""
+    seed = 2024
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    args = parse_args()
+    logger.info("=================================================")
+    logger.info(f"{args}")
+    logger.info("=================================================")
+
+    main(args.data_path, args.ckpt_path, args.output_file, torch.device(args.device))
